ca7f3278c466126c912cd9d16bbe40238a3ce418
Jeanne <jeanne.grenet@gmail.com>
2024-04-15 11:24:04 +0200

    error management for name, email and confirm email

--- src/SimpleForm.tsx
+++ src/SimpleForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import {
   FormControl,
   FormLabel,
@@ -27,32 +27,75 @@ export default function SimpleForm() {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm<IFormInput>();
   const onSubmit = handleSubmit((data) => console.log(data));
+  const watchEmail = watch("email");
   return (
     <form onSubmit={onSubmit}>
-      <FormControl maxW="50%" mx="auto" isRequired>
+      <FormControl
+        maxW="50%"
+        mx="auto"
+        isRequired
+        isInvalid={errors.name?.type === "required"}
+      >
         <FormLabel>Name</FormLabel>
-        <Input {...register("name", { required: true })} />
+        <Input
+          {...register("name", { required: "This input is required" })}
+          aria-invalid={errors.name ? "true" : "false"}
+        />
+        <FormErrorMessage>
+          {errors.name && errors.name.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl
+        maxW="50%"
+        mx="auto"
+        isRequired
+        isInvalid={errors.email?.type === "required"}
+      >
         <FormLabel>Email</FormLabel>
         <Input
           {...register("email", {
-            required: true,
+            required: "Email is required",
             pattern: {
               value: /\S+@\S+\.\S+/,
               message: "Entered value does not match email format",
             },
           })}
         />
-        {errors.email && <span role="alert">{errors.email.message}</span>}
+        <FormErrorMessage>
+          {errors.email && errors.email.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl
+        maxW="50%"
+        mx="auto"
+        isRequired
+        isInvalid={errors.confirmEmail?.type === "validate"}
+      >
         <FormLabel>Confirm email</FormLabel>
-        <Input {...register("confirmEmail")} />
+        <Input
+          {...register("confirmEmail", {
+            required: "Please confirm your email",
+            validate: (value) => value === watchEmail || "Emails do not match",
+          })}
+        />
+        <FormErrorMessage>
+          {errors.confirmEmail && errors.confirmEmail.message}
+        </FormErrorMessage>
+      </FormControl>
+
+      <FormControl maxW="50%" mx="auto">
         <FormLabel>Account type</FormLabel>
         <Select {...register("accountType")}>
           <option value="Personal">Personal</option>
           <option value="Professional">Professional</option>
         </Select>
       </FormControl>
+
       <Button mt={4} colorScheme="teal" type="submit">
         Submit
       </Button>
